5c7f24acd8bed240c54032a6b5f09d8295784142
JJ Idt <johnidt@gmail.com>
2014-08-04 22:27:27 -0700

    make a new instance of dice have a random number value

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,3 +1,9 @@
 class Dice
-  
+
+  attr_reader :number
+
+  def initialize
+    @number = Random.rand(1..6)
+  end
+
 end
--- spec/pig_spec.rb
+++ spec/pig_spec.rb
@@ -2,8 +2,14 @@ require 'rspec'
 require 'dice'
 
 describe 'Dice' do
-  it 'creates a single die' do
+  it 'creates a single instance of dice' do
     test_dice = Dice.new
     expect(test_dice).to be_an_instance_of Dice
   end
+
+  it 'initializes with a random number value between 1 and 6' do
+    test_dice = Dice.new
+    expect(test_dice.number).to be_an_instance_of Fixnum
+  end
+
 end
